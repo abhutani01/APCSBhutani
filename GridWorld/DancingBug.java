2e9161ba0f6e4e636ee19371e50f01398a033b09
abhutani01 <31668097+abhutani01@users.noreply.github.com>
2017-10-03 22:40:54 -0600

    Create DancingBug.java

---
+++ GridWorld/DancingBug.java
@@ -0,0 +1,32 @@
+
+package GridWorld;
+
+import java.awt.Color;
+
+import info.gridworld.actor.Bug;
+
+public class DancingBug extends Bug {
+
+	private int step;
+	private int x;
+
+	public DancingBug() {
+		step = 0;
+		setColor(Color.ORANGE);
+	}
+
+	public void turn(int x) {
+		x = (int) (Math.random() * 2) - 2;
+		for (int i = 1; i <= x; i++) {
+			turn();
+		}
+	}
+
+	public void act() {
+		if (step == x)
+			step = 0;
+		turn(x);
+		step++;
+		super.act();
+	}
+}
