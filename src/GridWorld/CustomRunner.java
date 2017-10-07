c63af9032e297995e5196d49758ac080933d3eba
abhutani01 <31668097+abhutani01@users.noreply.github.com>
2017-10-06 21:23:49 -0600

    Update CustomRunner.java

--- src/GridWorld/CustomRunner.java
+++ src/GridWorld/CustomRunner.java
@@ -1,5 +1,22 @@
 package GridWorld;
 
+
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.ActorWorld;
+import info.gridworld.actor.Bug;
+import info.gridworld.actor.Rock;
+import info.gridworld.grid.BoundedGrid;
+import info.gridworld.grid.Grid;
+
 public class CustomRunner {
+	public static void main(String[] args) {
+		Grid<Actor> grid = new BoundedGrid<Actor>(20, 20);
+		ActorWorld world = new ActorWorld(grid);
+		world.add(new Rock());
+		world.add(new Actor());
+		world.show();
+		world.add(new CustomBug(4));	
+
+	}
 
 }
