fe8bab9ca9fed1fb3fd788c7da56b7626e9142ed
abhutani01 <31668097+abhutani01@users.noreply.github.com>
2017-10-15 12:51:43 -0600

    Update Value.java

--- src/guess/Value.java
+++ src/guess/Value.java
@@ -1,28 +1,22 @@
 package guess;
 
 public class Value {
+	private static int min;
+	private static int max;
 	private int value;
-	private int min;
-	private int max;
 
-	/**
-	 * sets the Value to an integer
-	 * 
-	 * @param num
-	 */
-	public Value(int num) {
-		value = num;
-
+	public Value(int n, int m) {
+		min = n;
+		max = m;
 	}
-
 
 	/**
 	 * gets the max range value
 	 * 
 	 * @param m
 	 */
-	public void max(int m) {
-		max = m;
+	public static int getMax() {
+		return max;
 
 	}
 
@@ -31,57 +25,74 @@ public class Value {
 	 * 
 	 * @param n
 	 */
-	public void min(int n) {
-		min = n;
-
-	}
-
-	/**
-	 * gets the max range value
-	 * @return
-	 */
-	public int getMax() {
-		return max;
-	}
-	
-	/**
-	 * gets the min range value
-	 * @return
-	 */
-	public int getMin() {
+	public static int getMin() {
 		return min;
-	}
-	
-	/**
-	 * sets the Value to a random number in a given range
-	 */
-	public Value() {
-		value = (int) (Math.random() * getMax()) + getMin();
 
 	}
 
 	/**
-	 * gets the value
+	 * sets Value to random int between a given min and max
 	 * 
-	 * @return
-	 */
-	public int getValue() {
+	 * @param
+	 **/
+	public Value() {
+		max = Value.getMax();
+		min = Value.getMin();
+		value = (int) (Math.random() * max) + min;
+	}
+
+	/**
+	 * sets Value to an integer object
+	 * 
+	 * @param int
+	 **/
+	public Value(int num) {
+		value = num;
+	}
+
+	/**
+	 * Returns value of Value
+	 * 
+	 * @param
+	 * @return int
+	 **/
+	public int getVal() {
 		return value;
 	}
-	
 
+	/**
+	 * Checks if values are equal
+	 * 
+	 * @param value
+	 * @return boolean
+	 **/
+	public boolean equals(Value o) {
+		if (value == o.value) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+/**
+ * Checks if other value and value are equal, if other value is less than, or if other value is greater than
+ * @param o
+ * @return
+ */
 	public int compareTo(Value o) {
-		if (this.value == o.getValue()) {
+		if (value == o.value) {
 			return 0;
-		} else if (this.value < o.getValue()) {
+		} else if (value < o.value) {
 			return -1;
 		} else {
 			return 1;
 		}
 	}
 
+	/**
+	 * returns value as a string
+	 */
 	public String toString() {
 		return ("" + value);
 	}
 }
-
