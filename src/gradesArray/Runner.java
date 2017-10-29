c2de753e540dfca64acb4b969eead3c3535cc729
ArpitaBhutani <ArpitaBhutani@localhost>
2017-10-28 22:54:33 -0600

    code

--- src/gradesArray/Runner.java
+++ src/gradesArray/Runner.java
@@ -1,6 +1,5 @@
 package gradesArray;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Runner {
@@ -10,24 +9,38 @@ public class Runner {
 		Scanner rdr = new Scanner(System.in);
 
 		double[] grades = new double[3];
-		int count = 0;
 
 		for (int i = 0; i <= 2; i++) {
-			System.out.println("Enter three test scores from 0 to 100: ");
-			grades[i] = rdr.nextInt();
-			System.out.println(Arrays.toString(grades));
+			System.out.println("Enter a test score from 0 to 100: ");
+
+			try {
+				grades[i] = rdr.nextDouble();
+			}
+
+			catch (Exception e) {
+				System.out.println("Invalid - Enter a test score from 0 to 100: ");
+				rdr.next();
+				grades[i] = rdr.nextDouble();
+			}
+
+			while (grades[i] <= 0 || grades[i] >= 100) {
+				System.out.println("Out of Range - Enter a test score from 0 to 100: ");
+				grades[i] = rdr.nextDouble();
+			}
+
 		}
 		
+		System.out.println("The scores you input are: " + grades[0] + ", " + grades[1] + ", " + grades[2]);
 		System.out.println("The maximum score is: " + max(grades));
 		System.out.println("The minimum score is: " + min(grades));
 		System.out.println("The mean score is: " + mean(grades));
 		System.out.println("The median score is: " + median(grades));
-		
+		System.out.println("The mode score is: " + mode(grades));
 
 	}
 
 	public static double min(double[] a) {
-		if (a[0] < a[1] && a[0] < a[2])
+		if (a[0] < a[1] && a[0] < a[2] || (a[0] == a[1] && a[1] < a[2]))
 			return a[0];
 		else if (a[1] < a[2] && a[1] < a[0])
 			return a[1];
@@ -51,17 +64,32 @@ public class Runner {
 		return x;
 
 	}
-	
+
 	public static double median(double[] a) {
-		double x = min(a);
-		double y = max(a);
 		if (a[0] != min(a) && a[0] != max(a))
 			return a[0];
 		else if (a[1] != min(a) && a[1] != max(a))
 			return a[1];
-		else 
+		else
 			return a[2];
 
+	}
+
+	public static double mode(double[] a) {
+		double count = 0;
+		double count2 = 0;
+		for (int i = 0; i < a.length; i++) {
+			for (int x = i; x < a.length; x++) {
+				if (i != x && a[i] == a[x]) {
+					count++;
+					if (count >= count2) {
+						count = a[i];
+					}
+				}
+				count2 = 0;
+			}
+		}
+		return count;
 	}
 
 }
